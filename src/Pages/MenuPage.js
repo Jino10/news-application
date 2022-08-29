d75695bac067f2fa1498bcd817e3e7065ae7b54a
jino.x <jino.x@capestart.ngl>
2022-08-29 16:11:41 +0530

    menu page updated

--- src/Pages/MenuPage.js
+++ src/Pages/MenuPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import './../Styles/menu.css';
+
 function MenuPage() {
+    let data=useSelector(state=>state.user.userDetails);
     return (
+        <div className='wrapper_box'>
+            <p className='hello'>Hello {data.name}...</p>
         <div className='wrapper_contents'>
             <Link to="/home">
                 <div className='news_View'>
@@ -16,6 +21,7 @@ function MenuPage() {
                     <p className='heading'>WEATHER</p>
                 </div>
             </Link>
+        </div>
         </div>
     );
 }
